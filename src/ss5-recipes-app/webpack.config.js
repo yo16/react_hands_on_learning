9c57f388ab3555d35bdd76b8efa58d2f4d6cd5b4
yo16 <xxyo1xx@gmail.com>
2023-01-17 01:03:49 +0900

    add resource-map

--- src/ss5-recipes-app/webpack.config.js
+++ src/ss5-recipes-app/webpack.config.js
@@ -8,5 +8,6 @@ module.exports = {
     },
     module: {
         rules: [{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
-    }
+    },
+    devtool: "source-map"   // トップレベルにプロパティを追加
 };
