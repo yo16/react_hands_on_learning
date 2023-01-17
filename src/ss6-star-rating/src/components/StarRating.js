3244d905d3550a0d22a2569f219e9875f2a80bd5
yo16 <xxyo1xx@gmail.com>
2023-01-17 22:28:39 +0900

    add StarRating to App.js

--- src/ss6-star-rating/src/components/StarRating.js
+++ src/ss6-star-rating/src/components/StarRating.js
@@ -1,10 +1,20 @@
-import React from "react";
-import {FaStar} from "react-icons/fa";
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
 const Star = ({ selected = false }) => (
     <FaStar color={selected ? "red" : "grey" } />
 );
 
 export default function StarRating({ totalStars = 5 }) {
-    return [...Array(totalStars)].map((_, i) => <Star key={i} />);
+    const [selectedStars] = useState(3);
+    return (
+        <>
+            {[...Array(totalStars)].map((n, i) => (
+                <Star key={i} selected={selectedStars > i} />
+            ))}
+            <p>
+                {selectedStars} of {totalStars} stars
+            </p>
+        </>
+    );
 }
