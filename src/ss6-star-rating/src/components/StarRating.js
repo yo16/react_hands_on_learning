a4411e87ed7b6c0d4ee122d054492aa8bcc96edc
yo16 <xxyo1xx@gmail.com>
2023-01-17 22:20:21 +0900

    using Star Tag

--- src/ss6-star-rating/src/components/StarRating.js
+++ src/ss6-star-rating/src/components/StarRating.js
@@ -1,12 +1,10 @@
 import React from "react";
 import {FaStar} from "react-icons/fa";
 
-export default function StarRating() {
-    return [
-        <FaStar color="red" />,
-        <FaStar color="red" />,
-        <FaStar color="red" />,
-        <FaStar color="grey" />,
-        <FaStar color="grey" />
-    ]
+const Star = ({ selected = false }) => (
+    <FaStar color={selected ? "red" : "grey" } />
+);
+
+export default function StarRating({ totalStars = 5 }) {
+    return [...Array(totalStars)].map((_, i) => <Star key={i} />);
 }
