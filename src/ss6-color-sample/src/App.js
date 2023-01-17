b48ded7bba4f08ab2c6b76c26a7c9cabb61f8a9a
yo16 <xxyo1xx@gmail.com>
2023-01-18 01:04:51 +0900

    add input form

--- src/ss6-color-sample/src/App.js
+++ src/ss6-color-sample/src/App.js
@@ -1,20 +1,40 @@
 import {useState} from "react";
 import colorData from "./color-data.json";
 import ColorList from "./components/ColorList.js";
+import AddColorForm from "./components/AddColorForm.js";
+import { v4 } from "uuid";
 
 export default function App() {
   const [colors, setColors] = useState(colorData);
-  return <ColorList
-    colors={colors}
-    onRateColor={(id, rating) => {
-      const newColors = colors.map(color => 
-        color.id === id ? { ...color, rating } : color
-      );
-      setColors(newColors);
-    }}
-    onRemoveColor={id => {
-      const newColors = colors.filter(color => color.id !== id);
-      setColors(newColors);
-    }}
-  />;
+  return (
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title, 
+              color
+            }
+          ];
+          setColors(newColors);
+        }}
+      />
+      <ColorList
+        colors={colors}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map(color => 
+            color.id === id ? { ...color, rating } : color
+          );
+          setColors(newColors);
+        }}
+        onRemoveColor={id => {
+          const newColors = colors.filter(color => color.id !== id);
+          setColors(newColors);
+        }}
+      />
+    </>
+  );
 }
